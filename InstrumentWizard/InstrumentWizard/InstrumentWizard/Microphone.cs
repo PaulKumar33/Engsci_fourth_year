854ccf33b48ec36ab3d6ff09eeb9d494bf903482
PaulKumar33 <paul.kumar@mail.utoronto.ca>
2019-09-11 02:51:30 -0400

    commiting first commit audio wizard

---
+++ InstrumentWizard/InstrumentWizard/InstrumentWizard/Microphone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+
+namespace InstrumentWizard
+{
+    class Microphone
+    {
+        //this is the main class to access microphones for audio recording
+
+        public static WaveIn waveIn;
+        public Microphone()
+        {
+            //constructor
+
+        }
+
+        public static void InitMicrophone()
+        {
+            waveIn = new WaveIn();
+        }
+    }
+}
