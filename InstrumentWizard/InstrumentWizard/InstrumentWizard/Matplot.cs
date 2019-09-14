583a40704068bcf3cc23c0fb14ff03627822ce21
PaulKumar33 <paul.kumar@mail.utoronto.ca>
2019-09-14 12:47:26 -0400

    commit plotting util first commit'

---
+++ InstrumentWizard/InstrumentWizard/InstrumentWizard/Matplot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace InstrumentWizard
+{
+    class Matplot
+    {
+        DataTable mainTable;
+
+        StreamReader dataStream;
+        public Matplot(string dir)
+        {
+            if (dir == null)
+            {
+                throw new Exception("Data file passed in was null");
+            }
+            /*dir = System.IO(dir);
+            //general mathplot class constructor
+            dataStream = new StreamReader(@"{0}", dir);*/
+
+            mainTable = new DataTable();
+            mainTable.Columns.Add("X_Value", typeof(double));
+            mainTable.Columns.Add("Y_Value", typeof(double));
+        }
+    }
+}
--- InstrumentWizard/test_code/COMTest/COMTest/ComTest.cs
+++ InstrumentWizard/test_code/COMTest/COMTest/ComTest.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Web.Script.Serialization;
+//using System.Web.Script.Serialization;
+using System.IO.Ports;
 
 namespace COMTest
 {
@@ -53,22 +54,45 @@ namespace COMTest
 
         String[] devices = {"ATMega2560"};
         private static Dictionary<string, int> controllerLUT = new Dictionary<string,int>();
+        public static SerialPort port;
+        public static String[] COMPorts;
+        public static bool connectionBool;
         
 
         public ComTest(string DeviceType)
         {
             //initialize the class. initalize parameters based off of the controller being used
-            controllerLUT.Add("ATMega2560", 0);
+            //controllerLUT.Add("ATMega2560", 0);
+            connectionBool = false;
         }
 
         public void COMPortList()
         {
-
+            COMPorts = SerialPort.GetPortNames();
+            if (COMPorts.Length != 0)
+            {
+                foreach (string _port in COMPorts)
+                {
+                    Console.WriteLine("Comport: {0}", _port);
+                }
+            }
         }
 
-        public void ConnectDevice()
+        public void ConnectDevice(string comPort)
         {
+            try
+            {
+                SerialPort sp = new SerialPort(comPort, 9600, Parity.None,
+                    8, StopBits.One);
+                port = sp;
 
+                Console.WriteLine("Connection made to comport: {0}", comPort);
+                connectionBool = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception thrown");
+            }
         }
 
         public void Read()
@@ -85,12 +109,18 @@ namespace COMTest
         {
 
         }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Testing COM communication");
-        }
+            //System.Threading.Thread.Sleep(2000);
+            
+            ComTest test = new ComTest(null);
+            test.COMPortList();
+            string com = ComTest.COMPorts[0];
+            System.Threading.Thread.Sleep(2000);
+            test.ConnectDevice(com);
+            System.Threading.Thread.Sleep(2000);
 
-        
-    }
+        }
+    }    
 }
